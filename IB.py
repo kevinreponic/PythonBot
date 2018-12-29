9ed1d6458acd7cb6ab6bcaabdf8224c90ebe849d
Kevin <kevin@reponic.org>
2018-12-29 08:38:58 -0400

    IGBot Hashtag fix (close browser when done)

--- IB.py
+++ IB.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import firebase_admin
-from firebase_admin import credentials, db
 import time
 import json
 class InstagramBot: 
@@ -10,11 +8,6 @@ class InstagramBot:
         self.username = username
         self.password = password
         self.driver = webdriver.Firefox()
-        cred = credentials.Certificate('./ServiceKey/igbot-python-firebase-adminsdk-zxoyd-e30fb4aa6a.json')
-        firebase_admin.initialize_app(cred, {
-            'databaseURL' : 'https://igbot-python.firebaseio.com/'
-        })
-        self.ref = db.reference('/users')
         
        
     def closeBrowser(self) :
@@ -55,21 +48,13 @@ class InstagramBot:
                 time.sleep(4)
                 like_button = lambda: driver.find_element_by_xpath("//span[@aria-label='Like']")
                 like_button().click()
-                photo_username = driver.find_element_by_xpath("/html/body/span/section/main/div/div/article/header/div[2]/div[1]/div[1]/h2/a").get_attribute("innerHTML")
-                print("Username: "+photo_username)
-                aux = {
-                    "username" : photo_username,
-                    "test" : "true"
-                }
-                
-                self.ref.push(aux)
-
                 time.sleep(15)
             except Exception as e:
                 print(e)
                 time.sleep(2)
-    
+
 
 kevinIG = InstagramBot("testreponic123", 'Test123')
 kevinIG.login()
 kevinIG.like_photo("paris")
+kevinIG.closeBrowser()
